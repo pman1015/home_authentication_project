558ef74a20665e29226ea1b7b56796fc19b3c426
pman1015 <hurd1999@gmail.com>
2024-08-03 19:50:00 -0400

    update validate

--- authentication_server/util/validate_jwt.js
+++ authentication_server/util/validate_jwt.js
@@ -4,7 +4,17 @@ import jwt from "jsonwebtoken";
 export default async function validateJWT(token) {
 	config();
 	try {
-		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+		const decoded = jwt.verify(
+			token,
+			process.env.JWT_SECRET,
+			function (err, decoded) {
+				if (err) {
+					return null;
+				} else {
+					return decoded;
+				}
+			}
+		);
 		return decoded;
 	} catch (err) {
 		return null;
