f0d787d040f59f8bd48655a16de3c441739f78a2
pman1015 <hurd1999@gmail.com>
2024-08-03 19:31:53 -0400

    jwt updates

--- authentication_server/routes/authentication_router.js
+++ authentication_server/routes/authentication_router.js
@@ -1,74 +1,74 @@
-import express from 'express';
-import { sendMail } from '../controllers/email_controller.js';
-import * as user_controller from '../controllers/user_controller.js';
-import validateJWT from '../util/validate_jwt.js';
+import express from "express";
+import {sendMail} from "../controllers/email_controller.js";
+import * as user_controller from "../controllers/user_controller.js";
+import validateJWT from "../util/validate_jwt.js";
 
 const authentication_router = express.Router();
 authentication_router.use(express.json());
 
-authentication_router.post('/login', async (req, res) => {
-  var user = await user_controller.getUser(req.body, res);
-  if (res.statusCode !== 200) {
-    res.send('User not found');
-    return;
-  }
+authentication_router.post("/login", async (req, res) => {
+	var user = await user_controller.getUser(req.body, res);
+	if (res.statusCode !== 200) {
+		res.send("User not found");
+		return;
+	}
 
-  user_controller.validatePassword(req.body, res, user);
+	user_controller.validatePassword(req.body, res, user);
 });
 
-authentication_router.get('/resetPassword', async (req, res) => {
-  var user = await user_controller.lookUpUserbyEmail(req.body, res);
-  if (res.statusCode !== 200) {
-    res.send('User not found');
-    return;
-  }
-  const result = await sendMail(user.email, user.getResetJWT());
-  console.log(result);
-  res.send('Password reset:' + user.getResetJWT());
+authentication_router.get("/resetPassword", async (req, res) => {
+	var user = await user_controller.lookUpUserbyEmail(req.body, res);
+	if (res.statusCode !== 200) {
+		res.send("User not found");
+		return;
+	}
+	const result = await sendMail(user.email, user.getResetJWT());
+	console.log(result);
+	res.send("Password reset:" + user.getResetJWT());
 });
 
-authentication_router.post('/validateJWT', async (req, res) => {
-  var jwt = req.body.jwt;
-  if(jwt === undefined){
-    res.send('Bad Request: JWT is required');
-    return;
-  }else{
-    validateJWT(jwt).then((result) => {
-      if(result === null){
-        res.send('Invalid JWT');
-      }else{
-        res.send('Valid JWT');
-      }
-    });
-  }
+authentication_router.post("/validateJWT", async (req, res) => {
+	var jwt = req.body.jwt;
+	if (jwt === undefined) {
+		res.send("Bad Request: JWT is required");
+		return;
+	} else {
+		const decoded = await validateJWT(jwt);
+		console.log(decoded);
+		if (decoded === undefined) {
+			res.send("Invalid JWT");
+		} else {
+			res.send("Valid JWT");
+		}
+	}
 });
 
-authentication_router.post('/createUser', async (req, res) => {
-  var user = await user_controller.getUser(req.body, res);
-  console.log(res.statusCode);
+authentication_router.post("/createUser", async (req, res) => {
+	var user = await user_controller.getUser(req.body, res);
+	console.log(res.statusCode);
 
-  if (res.statusCode === 400) {
-    res.send('Bad Request');
-    return;
-  }
+	if (res.statusCode === 400) {
+		res.send("Bad Request");
+		return;
+	}
 
-  if (res.statusCode !== 404) {
-    console.log('404');
-    res.statusCode = 404;
-    res.send('User already exists');
-    return;
-  }
-  await user_controller.createUser(req, res);
-  if (res.statusCode === 200) {
-    res.send('User created');
-  } else {
-    res.send('User not created');
-    s;
-  }
+	if (res.statusCode !== 404) {
+		console.log("404");
+		res.statusCode = 404;
+		res.send("User already exists");
+		return;
+	}
+	await user_controller.createUser(req, res);
+	if (res.statusCode === 200) {
+		res.send("User created");
+	} else {
+		res.send("User not created");
+		s;
+	}
 });
 
-authentication_router.post('/resetPassword', async (req, res) => {});
+authentication_router.post("/resetPassword", async (req, res) => {});
 
-authentication_router.post('/updateUsername', async (req, res) => {});
+authentication_router.post("/updateUsername", async (req, res) => {});
 
 export default authentication_router;
--- authentication_server/util/validate_jwt.js
+++ authentication_server/util/validate_jwt.js
@@ -1,12 +1,12 @@
-import { config } from "dotenv";
+import {config} from "dotenv";
 import jwt from "jsonwebtoken";
 
-export default async function validateJWT(token){
-    config();
-    try{
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        return decoded;
-    }catch(err){
-        return null;
-    }
-};
+export default async function validateJWT(token) {
+	config();
+	try {
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+		return decoded;
+	} catch (err) {
+		return null;
+	}
+}
